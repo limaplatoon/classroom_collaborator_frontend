728675331b8ac348487eadce3731c4678b32ea7e
carole-ouedraogo <ouedca@gmail.com>
2020-08-08 12:30:54 -0400

    navbar logout added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,77 +1,115 @@
-import React, {useEffect, useState} from 'react'
-import {Link} from 'react-router-dom'
-import {Navbar as BootstrapNavbar} from 'react-bootstrap'
-import Nav from 'react-bootstrap/Nav'
-import Image from 'react-bootstrap/Image'
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-import Popover from 'react-bootstrap/Popover'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Navbar as BootstrapNavbar } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Image from "react-bootstrap/Image";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 import Notifications from "../Notifications/Notifications";
-import NotificationsAPI from '../../API/NotificationsAPI'
+import NotificationsAPI from "../../API/NotificationsAPI";
 
 import "./NavBar.css";
 import logo from "../../static/img/logoClassUp.png";
-import blankAvatar from "../../static/img/blank-avatar.png";
-import mailIcon from "../../static/img/mail.jpeg";
+import blankAvatar from "../../static/img/blank-avatar.svg";
+import bAvatar from "../../static/img/b-avatar.png";
+// import mailIcon from "../../static/img/mail.jpeg";
 import bellIcon from "../../static/img/bell.svg";
 import alertIcon from "../../static/img/bell_alert.svg";
+import logout from "../../static/img/logout.png";
+import calendar from "../../static/img/calendar.png";
+import classes from "../../static/img/classes.png";
 
 const Navbar = () => {
-  const [alerts, setAlerts] = useState([])
-  const now = new Date()
-  
+  const [alerts, setAlerts] = useState([]);
+  const now = new Date();
+
   const notificationTimer = (alert) => {
-    let timeDelta = (new Date(alert.event_time) - now)/(1000*60)
-    setTimeout(() => setAlerts(state => [...state, alert]), (timeDelta - 60) * 1000)
+    let timeDelta = (new Date(alert.event_time) - now) / (1000 * 60);
+    setTimeout(
+      () => setAlerts((state) => [...state, alert]),
+      (timeDelta - 60) * 1000
+    );
     // setTimeout(() => setAlerts(state => [...state, alert]), (2.5 * alert.id) * 1000)
-  }
-  
+  };
+
   const loadNotifications = async () => {
-    setAlerts([])
-    const response = await NotificationsAPI.getNotifications()
-    const responseJson = await response.json()
-    responseJson.map((alert) => notificationTimer(alert))
-  }
+    setAlerts([]);
+    const response = await NotificationsAPI.getNotifications();
+    const responseJson = await response.json();
+    responseJson.map((alert) => notificationTimer(alert));
+  };
 
   const handleNotificationItemClick = async (alertID) => {
-    const response = await NotificationsAPI.updateNotifications(alertID)
-    setAlerts([...alerts].filter(alert => alert.id !== alertID))
-  }
-  
-  
+    const response = await NotificationsAPI.updateNotifications(alertID);
+    setAlerts([...alerts].filter((alert) => alert.id !== alertID));
+  };
+
   useEffect(() => {
-    loadNotifications()
-  }, [])
-  
+    loadNotifications();
+  }, []);
+
   const popover = (
     <Popover id="popover-basic">
       <Popover.Content>
-        {(alerts.length > 0) ? <Notifications alerts={alerts} handleItemClick={handleNotificationItemClick}/> : <span>no notifications</span>}
+        {alerts.length > 0 ? (
+          <Notifications
+            alerts={alerts}
+            handleItemClick={handleNotificationItemClick}
+          />
+        ) : (
+          <span>no notifications</span>
+        )}
       </Popover.Content>
     </Popover>
   );
-
+  // bg="light" variant="light"
+  // style={{ color: "#3b5998" }}
   return (
     <div>
-      <BootstrapNavbar bg="light" variant="light">
+      <BootstrapNavbar class="navbar">
         <BootstrapNavbar.Brand>
-          <Link to='/' style={{ textDecoration: 'none' }}>
-            <Image className='navbarLogo' src={logo} roundedCircle />
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <Image className="navbarLogo" src={logo} roundedCircle />
           </Link>
         </BootstrapNavbar.Brand>
+
+        {/* <BootstrapNavbar.Brand href="#profile">
+          <Image className="navbarImage" src={blankAvatar} roundedCircle />
+        </BootstrapNavbar.Brand> */}
         <Nav className="mr-auto">
-          <Nav.Link as={Link} to='/my-calender'>My Calendar</Nav.Link>
-          <Nav.Link as={Link} to='/test'>My Classes</Nav.Link>
+          <Nav.Link as={Link} to="/my-calender" class="color">
+            MY CALENDAR
+            <Image className="navbarImage" src={calendar} />
+          </Nav.Link>
+          <Nav.Link as={Link} to="/test" class="color">
+            MY CLASSES
+            <Image className="navbarImage" src={classes} />
+          </Nav.Link>
         </Nav>
-        <BootstrapNavbar.Brand style={{cursor: 'pointer'}}>
-          <OverlayTrigger trigger="click" placement="bottom" overlay={popover} rootClose>
-            <Image className="navbarImage" src={(alerts.length > 0) ? alertIcon : bellIcon} />
+        <BootstrapNavbar.Brand style={{ cursor: "pointer" }}>
+          <OverlayTrigger
+            trigger="click"
+            placement="bottom"
+            overlay={popover}
+            rootClose
+          >
+            <Image
+              className="navbarImage"
+              src={alerts.length > 0 ? alertIcon : bellIcon}
+            />
           </OverlayTrigger>
         </BootstrapNavbar.Brand>
-        <BootstrapNavbar.Brand href="#messages">
-          <Image className="navbarImage" src={mailIcon} />
-        </BootstrapNavbar.Brand>
+
         <BootstrapNavbar.Brand href="#profile">
-          <Image className="navbarImage" src={blankAvatar} roundedCircle />
+          <Nav.Link as={Link} to="/profile">
+            <Image className="navbarImage" src={bAvatar} roundedCircle />
+          </Nav.Link>
+        </BootstrapNavbar.Brand>
+
+        <BootstrapNavbar.Brand href="#logout">
+          <Nav.Link as={Link} to="/logout">
+            <Image className="navbarImage" src={logout} />
+          </Nav.Link>
         </BootstrapNavbar.Brand>
       </BootstrapNavbar>
     </div>
